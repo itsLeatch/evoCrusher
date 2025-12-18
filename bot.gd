e38f1189c82a2f517e773a0a7ea14526b57b7a93
Sebastian Rentsch <seb.rentsch@online.de>
2025-12-18 16:30:07 +0100

    spawn a single sensor

--- bot.gd
+++ bot.gd
@@ -7,6 +7,9 @@ var energy: float = 0
 
 # Called when the node enters the scene tree for the first time.
 var rng = RandomNumberGenerator.new()
+var hitResult;
+var sensorDistance : float = 100
+
 func _ready() -> void:
 	moveDirection.data = [1,1]
 	var randomWeightData = PackedFloat32Array()
@@ -16,17 +19,34 @@ func _ready() -> void:
 	weights.data = randomWeightData
 	#print(weights.data)
 	global_position = Vector2(rng.randf_range(-Global.mapSize.x / 2,Global.mapSize.x / 2), rng.randf_range(-Global.mapSize.y / 2,Global.mapSize.y / 2))
- 	
+ 
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	var result = weights.mul(moveDirection)
 	var resVelocity = Vector2(result.get_value(Vector2(0,0)), result.get_value((Vector2(0,1))))
 	#normalize
 	resVelocity /= resVelocity.length()
+	#rotation = velocity.angle() + PI /2
 	velocity = resVelocity * speed
-	$BotImage.rotate(1*delta)
 	move_and_slide()
 	$Label.text = str(energy)
+	
+	#ray cast
+	var space_state = get_world_2d().direct_space_state
+	var query = PhysicsRayQueryParameters2D.create(global_position, global_position + velocity / velocity.length() * sensorDistance, collision_mask)
+	hitResult = space_state.intersect_ray(query)
+	#if hitResult:
+	var hitVertices: PackedVector2Array;
+	hitVertices.append(Vector2(0,0))
+	if hitResult:
+		hitVertices.append(hitResult.position - global_position)
+		$Line2D.default_color = Color.RED
+	
+	else:
+		hitVertices.append(velocity / velocity.length() * sensorDistance)
+		$Line2D.default_color = Color.GREEN
+	$Line2D.points = hitVertices
 
 func incrementFood() ->void:
 	energy+=1
